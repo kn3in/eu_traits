84aba0a478eccab066635939c599f37daf0f3df1
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2014-06-25 17:02:28 +1000

    biolerplate

--- europe.js
+++ europe.js
@@ -29,7 +29,7 @@ var projection = d3.geo.mercator()
                    .center([13, 50])
                    .rotate([0, 0, 0])
                    .scale(600)
-                   .translate([w / 2, h / 2]);
+                   // .translate([w / 2, h / 2]);
 
 // Define default path generator
 var path = d3.geo.path()
@@ -77,27 +77,29 @@ var xAxis = d3.svg.axis()
 
 d3.json("final_tj.json", function(error, europe) {
     var subunits = topojson.feature(europe, europe.objects.subunits);
-    
-    height_obs_min = d3.min(subunits.features, function(d) { return parseFloat(d.properties.mean_height_obs); });
-    height_obs_max = d3.max(subunits.features, function(d) { return parseFloat(d.properties.mean_height_obs); });
-    bmi_obs_min = d3.min(subunits.features, function(d) { return parseFloat(d.properties.mean_bmi_obs); });
-    bmi_obs_max = d3.max(subunits.features, function(d) { return parseFloat(d.properties.mean_bmi_obs); });
-    height_est_min = d3.min(subunits.features, function(d) { return parseFloat(d.properties.mean_height_est); });
-    height_est_max = d3.max(subunits.features, function(d) { return parseFloat(d.properties.mean_height_est); });
-    bmi_est_min = d3.min(subunits.features, function(d) { return parseFloat(d.properties.mean_bmi_est); });
-    bmi_est_max = d3.max(subunits.features, function(d) { return parseFloat(d.properties.mean_bmi_est); });
+    var traits = ["mean_height_est", "mean_height_obs", "mean_bmi_est", "mean_bmi_obs"]
 
+    function draw_this(trait) {
+     // Define domain
+     trait_min = d3.min(subunits.features, function(d) { return parseFloat(d.properties.trait); });
+     trait_max = d3.max(subunits.features, function(d) { return parseFloat(d.properties.trait); });
+     // domain
+     color.domain([trait_min, trait_max]);
+     x.domain([trait_min, trait_max]);
+     // axis
+     g.call(xAxis)
+      .append("text")
+      .attr("class", "caption")
+      .attr("y", -6)
+      .text(look_up[trait]);
 
-    color.domain([height_est_min, height_est_max]);
-    x.domain([height_est_min, height_est_max]);
-
-    svg.selectAll("path")
+     svg.selectAll("path")
        .data(subunits.features)
        .enter().append("path")
        .attr("class", "country")
        .attr("d", path)
        .style("fill", function(d) {
-                     var value1 = d.properties.mean_height_est;
+                     var value1 = d.properties[trait];
                      if(value1) {
                       return color(value1);
                     } else {
@@ -105,66 +107,8 @@ d3.json("final_tj.json", function(error, europe) {
                     }
 
       })
-      
-      g.call(xAxis)
-     .append("text")
-     .attr("class", "caption")
-     .attr("y", -6)
-     .text(look_up["mean_height_est"]);
-});
+    }
 
-// -----------------------------------------------------------------------------------------------------------------
+    traits.each(trait draw_this(trait));
 
-d3.select("select").on("change", function() {
-      var trait = this.value;
-
-      // Update domains
-      // When one would like to compare observed vs estimated
-      // on the same color scale, keep domain from
-      // observed data cause estimated domain will be smaller
-      // however it will make it more difficult to see
-      // for height that observed and estimated patterns follow each other
-      // if(trait.valueOf() == "mean_height_est" || trait.valueOf() == "mean_height_obs") {
-      //   color.domain([height_max, height_min]);
-      //   x.domain([height_max, height_min]);
-      // } else {
-      //   color.domain([bmi_max, bmi_min]);
-      //   x.domain([bmi_max, bmi_min]);
-      // }
-      
-      // Update domain
-      switch(trait.valueOf()) {
-        case "mean_height_est" :
-          color.domain([height_est_min, height_est_max]);
-          x.domain([height_est_min, height_est_max]);
-          break;
-        case "mean_height_obs" :
-          color.domain([height_obs_min, height_obs_max]);
-          x.domain([height_obs_min, height_obs_max]);
-          break;
-        case "mean_bmi_est" :
-          color.domain([bmi_est_min, bmi_est_max]);
-          x.domain([bmi_est_min, bmi_est_max]);
-          break;
-        case "mean_bmi_obs" :
-          color.domain([bmi_obs_min, bmi_obs_max]);
-          x.domain([bmi_obs_min, bmi_obs_max]);
-          break;
-      }
-
-      // update axis 
-      g.call(xAxis)
-       .selectAll("text.caption")
-       .text(look_up[trait]);
-      
-      // update chrolopleth
-      svg.selectAll("path.country")
-         .style("fill",  function(d) {
-                          var value = d.properties[trait];
-                          if (value) {
-                                  return color(value);
-                          } else {
-                                  return "#f0f0f0";
-                          }
-      })
 });
