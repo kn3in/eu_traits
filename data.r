4f681c47e9e8f39af476e7f292246c9db7cf7f3a
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2015-06-26 09:55:47 +1000

    swaped old data for new data

--- data.r
+++ data.r
@@ -5,6 +5,35 @@ mcmcdata <- list(mean_bmi_obs = obs_bmi,
               mean_height_obs = obs_ht,
                  mean_bmi_est = bmi_means,
               mean_height_est = ht_means)
+#---------------------------------------------------------------
+new_height <- read.table("map_height.txt")
+new_bmi <- read.table("map_bmi.txt")
+
+new_means_height <- plyr::ddply(new_height, "V2", function(x) {
+	mean(x$V1)
+})
+
+new_means_bmi <- plyr::ddply(new_bmi, "V2", function(x) {
+	mean(x$V1)
+})
+
+colnames(new_means_height)[2] <- "mean_height_est"
+new_means_height$ccode <- countrycode(new_means_height$V2, origin = "iso3c", destination = "country.name")
+
+colnames(new_means_bmi)[2] <- "mean_bmi_est"
+new_means_bmi$ccode <- countrycode(new_means_bmi$V2, origin = "iso3c", destination = "country.name")
+
+new_dt <- merge(new_means_bmi[ ,-1], new_means_height[ ,-1], by = "ccode")
+
+tmpnew <- plyr::ldply(c("England", "Scotland", "Wales"), function(name) {
+	tmp <- new_dt[new_dt$ccode == "United Kingdom", ]
+    tmp$ccode <- name
+    tmp
+})
+
+new_dt <- as.data.frame(rbind(new_dt, tmpnew))
+#---------------------------------------------------------------
+
 mcmcdata <- lapply(mcmcdata, colMeans)
 lapply(mcmcdata, function(x) all.equal(names(x), names(mcmcdata[[1]])))
 mcmcdata <- as.data.frame(do.call(cbind, mcmcdata))
@@ -29,5 +58,40 @@ tmp <- plyr::ldply(c("England", "Scotland", "Wales"), function(name) {
 mcmcdata <- as.data.frame(rbind(mcmcdata, tmp))
 rownames(mcmcdata) <- NULL
 mcmcdata <- mcmcdata[ ,colnames(dummy)]
-
+#---------------------------------------------------------------
+mcmcdata
+new_dt
+mcmcdata <- merge(mcmcdata[ ,-c(4,5)], new_dt, by = "ccode")
+mcmcdata <- mcmcdata[ ,colnames(dummy)]
 write.csv(mcmcdata, file = "real_data.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
